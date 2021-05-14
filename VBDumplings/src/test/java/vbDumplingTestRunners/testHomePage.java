9623e1852bfc4850b2b5a619916f3dc68c4ab970
Rahul Pradhan <Rahul Pradhan@RAHUL-LAPTOP>
2021-05-14 15:57:44 +0530

    Added exception handling to testHomePage.java

--- VBDumplings/src/test/java/vbDumplingTestRunners/testHomePage.java
+++ VBDumplings/src/test/java/vbDumplingTestRunners/testHomePage.java
@@ -40,72 +40,126 @@ public class testHomePage{
 	
 	@Test(priority=0)
 	public void checkTitleText() {
-		Assert.assertEquals(objHomePage.getHeaderText().toUpperCase(),expectedTitleText.toUpperCase());
-		System.out.println("[TEST RUN] HomePage: Title Text Pass");
+		try {
+			Assert.assertEquals(objHomePage.getHeaderText().toUpperCase(),expectedTitleText.toUpperCase());
+			System.out.println("[TEST RUN] HomePage: Title Text Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Title Text Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=1)
 	public void checkDescriptionText() {
-		Assert.assertEquals(objHomePage.getDescriptionText(),expectedDescriptionText);
-		System.out.println("[TEST RUN] HomePage: Title Description Text Pass");
+		try {
+			Assert.assertEquals(objHomePage.getDescriptionText(),expectedDescriptionText);
+			System.out.println("[TEST RUN] HomePage: Title Description Text Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: itle Description Text Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=3)
 	public void checkHomePageLink() {
-		objHomePage.clickHomePageLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedHomePageTitle);
-		System.out.println("[TEST RUN] HomePage: Title 'Home' Page Link Pass");
+		try {
+			objHomePage.clickHomePageLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedHomePageTitle);
+			System.out.println("[TEST RUN] HomePage: Title 'Home' Page Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Title 'Home' Page Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=4)
 	public void checkMenuHomeLink() {
-		objHomePage.clickMenuHomeLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedHomePageTitle);
-		System.out.println("[TEST RUN] HomePage: Menu 'Home' Page Link Pass");
+		try {
+			objHomePage.clickMenuHomeLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedHomePageTitle);
+			System.out.println("[TEST RUN] HomePage: Menu 'Home' Page Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Menu 'Home' Page Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=5)
 	public void checkMenuMenuLink() {
-		objHomePage.clickMenuMenuLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedMenuPageTitle);
-		System.out.println("[TEST RUN] HomePage: Menu 'Menu' Link Pass");
+		try {
+			objHomePage.clickMenuMenuLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedMenuPageTitle);
+			System.out.println("[TEST RUN] HomePage: Menu 'Menu' Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Menu 'Menu' Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=6)
 	public void checkMenuFAQLink() {
-		objHomePage.clickMenuFAQLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedFAQPageTitle);
-		System.out.println("[TEST RUN] HomePage: Menu 'FAQs' Link Pass");
+		try {
+			objHomePage.clickMenuFAQLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedFAQPageTitle);
+			System.out.println("[TEST RUN] HomePage: Menu 'FAQs' Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Menu 'FAQs' Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=7)
 	public void checkMenuTestimonialsLink() {
-		objHomePage.clickMenuTestimonialsLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedTestimonialsPageTitle);
-		System.out.println("[TEST RUN] HomePage: Menu 'Testimonials' Link Pass");
+		try {
+			objHomePage.clickMenuTestimonialsLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedTestimonialsPageTitle);
+			System.out.println("[TEST RUN] HomePage: Menu 'Testimonials' Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Menu 'Testimonials' Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=8)
 	public void checkMenuAboutLink() {
-		objHomePage.clickMenuAboutLink();
-		Assert.assertEquals(objHomePage.getPageTitle(), expectedAboutPageTitle);
-		System.out.println("[TEST RUN] HomePage: Menu 'About' Link Pass");
+		try {
+			objHomePage.clickMenuAboutLink();
+			Assert.assertEquals(objHomePage.getPageTitle(), expectedAboutPageTitle);
+			System.out.println("[TEST RUN] HomePage: Menu 'About' Link Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: Menu 'About' Link Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=9)
 	public void checkLHSHeaderText() {
-		objHomePage.clickMenuHomeLink();
-		Assert.assertEquals(objHomePage.getLHSHeaderText().toUpperCase(),expectedLHSHeaderText.toUpperCase());
-		System.out.println("[TEST RUN] HomePage: LHS Header Text Pass");
+		try {
+			objHomePage.clickMenuHomeLink();
+			Assert.assertEquals(objHomePage.getLHSHeaderText().toUpperCase(),expectedLHSHeaderText.toUpperCase());
+			System.out.println("[TEST RUN] HomePage: LHS Header Text Pass");
+		}
+		catch(AssertionError ae) {
+			System.out.println("[TEST RUN] HomePage: LHS Header Text Fail (" + ae.getMessage() + ")");
+			Assert.assertTrue(false);
+		}
 	}
 	
 	@Test(priority=10)
 	public void checkMenuOrderLink() {
-		//Store the ID of the original window
-		String originalWindow = driver.getWindowHandle();	
-		objHomePage.clickMenuOrderLink();		
-		objHomePage.verifyOrderPageHeader(originalWindow, expectedOrderPageHeader);
-		System.out.println("[TEST RUN] HomePage: Menu 'Order' Link Pass");
+			//Store the ID of the original window
+			String originalWindow = driver.getWindowHandle();	
+			objHomePage.clickMenuOrderLink();		
+			objHomePage.verifyOrderPageHeader(originalWindow, expectedOrderPageHeader);
+			System.out.println("[TEST RUN] HomePage: Menu 'Order' Link Pass");
 	}
 	
 	@Test(priority=11)
